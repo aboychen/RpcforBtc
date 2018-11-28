5ad757c895fa0161b3a7690d2a74540999d74896
mnoumanuaar32xc <mnoumanuaar@yahoo.com>
2018-11-28 13:11:23 +0500

    a

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ console.log('getbalance');
  }
 
  app.post('/GetNewAddress',function(req,res){
-    var accountName = req.query.accountName;
+    var accountName = req.body.accountName;
     const batch = [
         { method: 'getnewaddress', params: [accountName] }
       ]
