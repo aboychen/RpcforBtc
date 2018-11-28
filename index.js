deff69af820b9f5643d1163d7431f455f2083951
mnoumanuaar32xc <mnoumanuaar@yahoo.com>
2018-11-28 15:02:13 +0500

    e

--- index.js
+++ index.js
@@ -1,55 +1,57 @@
-const express = require ('express')
+const express = require('express')
 const app = express()
 const Client = require('bitcoin-core');
-//const client = new Client({ network: 'regtest' });
-// const client_c = new Client({ port: 8332 });
-// //127.0.0.1
+const bodyParser  =require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // const fs = require('fs');
 const client = new Client({
-    username:'bitcoinN0deAccess',
-      password:'bitc0inRPCpass',
-      port: 8332,
-  });
+    username: 'bitcoinN0deAccess',
+    password: 'bitc0inRPCpass',
+    port: 8332,
+});
 //   const client = new Client({ headers: true });
 //  console.log( client.getInfo())
-app.post('/getInfo',function(req,res){
- client.getInfo();
+app.post('/getInfo', function (req, res) {
+    client.getInfo();
     res.send(client.getInfo());
-  })
-app.post('/getbalance')
-const getbalance=(req,res)=>{
-console.log('getbalance');
- console.log( client.getbalance());
+})
+app.post('/login',function(req,res){
+    var user_name=req.body.user;
+    var password=req.body.password;
+    console.log("User name = "+user_name+", password is "+password);
+    res.end("yes");
+  });
+  app.post('/GetNewAddress',function(req,res){
+    var accName=req.body.accountName;
+   
+     const batch = [
+        { method: 'getnewaddress', params: [accName] }
+      ]
 
- }
-
- app.post('/GetNewAddress',function(req,res){
-   // var accountName = req.query.accountName;
-    console.log(req.query.accountName);
-    console.log(req.body.accountName);
-    // const batch = [
-    //     { method: 'getnewaddress', params: [accountName] }
-    //   ]
+       client.command(batch).then(([address, error]) =>res.send(address), console.log(accountName) );
         
-    //    client.command(batch).then(([address, error]) =>res.send(address), console.log(accountName) );
+    console.log("User name = "+accName+", password is ");
+       res.end("yes");
+  });
 
- });
-
- app.post('/getaccount',function(req,res){
-    var accountNo=req.query.accountNo;
+app.post('/getaccount', function (req, res) {
+    var accountNo = req.query.accountNo;
     const batch = [
         { method: 'getaccount', params: [accountNo] }
-      ]
-        
-       client.command(batch).then(([address, error]) =>res.send(address) );
- });
-app.post('/getaddressesbyaccount',function(req,res){
-    var accountNo=req.query.accountNo;
+    ]
+
+    client.command(batch).then(([address, error]) => res.send(address));
+});
+app.post('/getaddressesbyaccount', function (req, res) {
+    var accountNo = req.query.accountNo;
     const batch = [
         { method: 'getaddressesbyaccount', params: [accountNo] }
-      ]
-        
-       client.command(batch).then(([address, error]) =>res.send(address) );
+    ]
+
+    client.command(batch).then(([address, error]) => res.send(address));
 });
- app.listen(3000,()=> console.log("Server is running at http://localhost:3000"));
+app.listen(3000, () => console.log("Server is running at http://localhost:3000"));
 
